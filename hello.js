5092df69bdc54aa16be576bc223b3aec813f760b
ryanroessner <ryan.roessner@gmail.com>
2015-08-13 21:19:46 -0400

    clear files

--- hello.js
+++ hello.js
@@ -1,19 +1,2 @@
 // this is a javascript file
-
-var hello = "hola"
-
-var taco = "awesome"
-
-var mybrain = "tired"
-
-var myeyes = "bleary"
-
-var mynose = "snotty"
-
-var myarms = "weak"
-
-var variable = "var"
-
-// changed project test
-
 // testing git
